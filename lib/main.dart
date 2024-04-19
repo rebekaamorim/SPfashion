9becafff4e0685ab100b7cc8355a3c4732c2e915
rebekaamorim <rebeka.amorim@etec.sp.gov.br>
2024-04-18 21:33:35 -0300

    reset feito e recomposto

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,7 @@ import 'dart:ui';
 import 'package:aplicacao_spfashion/eventos.dart';
 import 'package:aplicacao_spfashion/widgets/input_decoration.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/semantics.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -16,44 +12,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
        debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        //fundo da tela login
-        body: Container(
-          height: double.infinity,
-          //adicionando fundo
-          decoration: const BoxDecoration(
-            image:DecorationImage(
-              image: AssetImage('assets/imgs/fungo_colorido.png'),
-              fit:BoxFit.cover,
-          )
-          ),
-          
-          //Stack : colocar widget em cima do outro
-          child: Stack(
-            //nessa children é chamado os métodos dos widgets
-            children: [
-              loginform(context)
-            ],
-          )
-        ),
-      ),
+      home: Login()
     );
   }
+}
 
-  SingleChildScrollView loginform(BuildContext context) {
-    return SingleChildScrollView(
-      child:Column(
-              children: [
-                //SizedBox: altura que o widget fica
-                //const em tudo que for construtor
-                const SizedBox( height: 250),
-                Container(
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/imgs/fungo_colorido.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 250),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
                   padding: const EdgeInsets.all(20),
                   margin: const EdgeInsets.symmetric(horizontal: 30),
-                  width:double.infinity,
-                  height: 450,
                   decoration: BoxDecoration(
-                    color:Color.fromARGB(143, 255, 255, 255),
+                    color: Color.fromARGB(143, 255, 255, 255),
                     borderRadius: BorderRadius.circular(25),
                     boxShadow: const [
                       BoxShadow(
@@ -61,77 +45,89 @@ class MyApp extends StatelessWidget {
                         blurRadius: 15,
                         offset: Offset(0, 5),
                       )
-                    ] ),
-                // aqui tem o conteúdo dentro do container de fundo
-                child: Column(
-                  children: [
-                    //posição de cada entrada de texto
-                    const SizedBox(height:10,),
-                    //titulo login
-                    const Text('Login', 
-                          style: TextStyle(
-                            fontFamily: 'Chloe',
-                            fontSize: 40,
-                            color: Colors.deepOrange
-                          ),
-                    ),
-                    const SizedBox(height:40),
-                    //container que tem todas as informações e botão
-                    Container(
-                      child: Form(child: Column(
-                        children: [
-                          //aqui são os texts que vão receber dados
-                          TextFormField(
-                            autocorrect: false,
-                            //aqui é chamado a classe InputDecorations, utilizando seu método para receber os valores e tornar o código mais limpo
-                            //A classe InputDecorations está presente no endereço 'lib/widgets/inputdecoration.dart'
-                            decoration: InputDecorations.inputDecoration(hinttext: 'user123', labeltext: 'Nome usuário', icone: const Icon(Icons.person_2_sharp, color: Colors.deepPurple,), )
-                          ),
-                          const SizedBox(height:30),
-                          TextFormField(
-                            autocorrect: false,
-                            obscureText: true,
-                            decoration: InputDecorations.inputDecoration(hinttext: '********', labeltext: 'Senha', icone:const Icon(Icons.lock_outline, color: Colors.deepPurple))),
-                          const SizedBox(height:30),
-                          //nao tenho conta, Row alinha ela para o fim (esquerda) do container
-                          const Row(
-                            mainAxisAlignment: MainAxisAlignment.end, 
-                            children: [Text('Não tenho uma conta!', 
-                            style: TextStyle(
-                              color: Colors.deepOrange,
-                              fontSize: 15, 
-                              fontWeight: FontWeight.bold),),],),
-                        
-                          const SizedBox(height:30),
-                          MaterialButton(
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                            //quando o botão tiver desabilitado(caso não for preenchida aas informações)
-                            disabledColor: Colors.grey,
-                            //quando tiver habilitado
-                            color:Colors.deepOrange,
-                            //Estilizando o nome do botão, por isso colocar o container
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 80,
-                                vertical: 10),
-                              child: const Text(
-                                'Entrar', 
-                                style: TextStyle(
-                                  color: Colors.white,
-                                   fontSize: 20,))),
-                            onPressed: () {} ,
-
-                            
-                          )
-                        ],
-                      ),),
-                    )
-                  
                     ],
-                  ),),
-                
-              ],
-    ),);
+                  ),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Login',
+                        style: TextStyle(
+                          fontFamily: 'Chloe',
+                          fontSize: 40,
+                          color: Colors.deepOrange,
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      Container(
+                        child: Form(
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                autocorrect: false,
+                                decoration: InputDecorations.inputDecoration(
+                                  hinttext: 'user123',
+                                  labeltext: 'Nome usuário',
+                                  icone: const Icon(Icons.person_2_sharp, color: Colors.deepPurple),
+                                ),
+                              ),
+                              const SizedBox(height: 30),
+                              TextFormField(
+                                autocorrect: false,
+                                obscureText: true,
+                                decoration: InputDecorations.inputDecoration(
+                                  hinttext: '********',
+                                  labeltext: 'Senha',
+                                  icone: const Icon(Icons.lock_outline, color: Colors.deepPurple),
+                                ),
+                              ),
+                              const SizedBox(height: 30),
+                              const Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    'Não tenho uma conta!',
+                                    style: TextStyle(
+                                      color: Colors.deepOrange,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 30),
+                              MaterialButton(
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                                disabledColor: Colors.grey,
+                                color: Colors.deepOrange,
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+                                  child: const Text(
+                                    'Entrar',
+                                    style: TextStyle(color: Colors.white, fontSize: 20),
+                                  ),
+                                ),
+                                onPressed: () {
+                                   Navigator.push(
+                    context,
+                    MaterialPageRoute(builder:
+                       (context) => const Eventos()),
+                         );
+                                },
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
