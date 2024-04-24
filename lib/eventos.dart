977a415bd66d1d1e5cc6c0d1ecafca698e2835d2
rebekaamorim <rebeka.amorim@etec.sp.gov.br>
2024-04-23 22:42:25 -0300

    andamento sobre o evento e eventos termindo

--- lib/eventos.dart
+++ lib/eventos.dart
@@ -1,10 +1,9 @@
 import 'dart:html';
-
-
+import 'package:aplicacao_spfashion/sobre_evento.dart';
 import 'package:flutter/material.dart';
 
 
-void eventos() => runApp(Eventos());
+void eventos() => runApp(const Eventos());
 
 class Eventos extends StatelessWidget {
   const Eventos({Key? key}) : super(key: key); // Correção: Adicionado o parâmetro 'key' e seu uso corretamente
@@ -41,7 +40,7 @@ class Eventos extends StatelessWidget {
                 ),)
                
               ),
-            SizedBox(width: 50,),
+            const SizedBox(width: 50,),
             const Text(
               'Eventos de moda',
               style: TextStyle(
@@ -52,48 +51,62 @@ class Eventos extends StatelessWidget {
             ),
             ],),
             
-            const SizedBox(height: 50), // Correção: Removido vírgula extra
-            Container(
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.symmetric(horizontal: 30),
-              width: double.infinity,
-              height: 140,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [Color.fromARGB(255, 192, 115, 255),Color.fromARGB(255, 255, 190, 115), Color.fromARGB(172, 255, 218, 218)]),
-                borderRadius: BorderRadius.circular(25),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 15,
-                    offset: Offset(0, 5),
-                  )
-                ],
-              ),
-               child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'São Paulo Fashion Week',
-                    style: TextStyle(
-                      fontFamily: 'Chloe',
-                      fontSize: 24,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+            const SizedBox(height: 30,),
+               GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(builder:
+                       (context) => Sobre()));
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    margin: const EdgeInsets.symmetric(horizontal: 30),
+                    width: double.infinity,
+                    height: 140,
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 192, 115, 255),
+                          Color.fromARGB(255, 255, 190, 115),
+                          Color.fromARGB(172, 255, 218, 218),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(25),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 15,
+                          offset: Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'São Paulo Fashion Week',
+                          style: TextStyle(
+                            fontFamily: 'Chloe',
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'A Fashion Week é um momento emocionante na indústria da moda...',
+                          style: TextStyle(
+                            fontFamily: 'Glacial',
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    'A Fashion Week é um momento emocionante na indústria da moda...',
-                    style: TextStyle(
-                      fontFamily: 'Glacial',
-                      fontSize: 18,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              
-            ),
+                ),
+          
             const SizedBox(height: 30,),
             Container(
               padding: const EdgeInsets.all(20),
@@ -228,3 +241,4 @@ class Eventos extends StatelessWidget {
     );
   }
 }
+
